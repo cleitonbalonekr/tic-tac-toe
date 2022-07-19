f9ca79e9f6a02bf1ef064d5b14fe5aeb211259d3
cleitonbalonekr <cleitonbaloneker@gmail.com>
2022-07-19 19:59:43 -0300

    Feat: add history

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,17 @@
 import { useEffect, useState } from 'react';
 import './index.css';
 
+enum PlayerSymbol {
+  PLAYER1 = 'X',
+  PLAYER2 = 'O',
+  DRAW = 'E',
+}
+
 function App() {
   const emptyBoard = Array(9).fill('');
   const [board, setBoard] = useState(emptyBoard);
-  const [currentPlayer, setCurrentPlayer] = useState('X');
+  const [history, setHistory] = useState<string[]>([]);
+  const [currentPlayer, setCurrentPlayer] = useState(PlayerSymbol.PLAYER1);
   const [winner, setWinner] = useState<string | null>(null);
 
   useEffect(verifyWinner, [board]);
@@ -22,7 +29,11 @@ function App() {
       index === itemIndex ? currentPlayer : item
     );
     setBoard(updatedBoard);
-    setCurrentPlayer(currentPlayer === 'X' ? 'O' : 'X');
+    setCurrentPlayer(
+      currentPlayer === PlayerSymbol.PLAYER1
+        ? PlayerSymbol.PLAYER2
+        : PlayerSymbol.PLAYER1
+    );
   }
 
   function verifyWinner() {
@@ -41,11 +52,13 @@ function App() {
     ];
 
     possibleWaysToWin.forEach((cells) => {
-      if (cells.every((cell) => cell === 'X')) {
-        setWinner('X');
+      if (cells.every((cell) => cell === PlayerSymbol.PLAYER1)) {
+        setWinner(PlayerSymbol.PLAYER1);
+        setHistory([...history, PlayerSymbol.PLAYER1]);
       }
-      if (cells.every((cell) => cell === 'O')) {
-        setWinner('O');
+      if (cells.every((cell) => cell === PlayerSymbol.PLAYER2)) {
+        setWinner(PlayerSymbol.PLAYER2);
+        setHistory([...history, PlayerSymbol.PLAYER2]);
       }
     });
     checkDraw();
@@ -53,46 +66,89 @@ function App() {
 
   function checkDraw() {
     if (board.every((items) => items !== '')) {
-      setWinner('E');
+      setWinner(PlayerSymbol.DRAW);
+      setHistory([...history, PlayerSymbol.DRAW]);
     }
   }
 
   function resetGame() {
-    setCurrentPlayer('X');
+    const random = Math.random() * 100;
+    const nextPlayer =
+      random > 50 ? PlayerSymbol.PLAYER1 : PlayerSymbol.PLAYER2;
+    console.log('random', random);
+    console.log('nextPlayer', nextPlayer);
+    setCurrentPlayer(nextPlayer);
     setBoard(emptyBoard);
     setWinner(null);
   }
 
+  function getTotalVictory(player: PlayerSymbol) {
+    return history.filter((hist) => hist === player).length;
+  }
+
   return (
     <main>
-      <h1 className="title">Jogo da Velha</h1>
-      <div className={`board ${winner && 'game-over'}`}>
-        {board.map((item, index) => (
-          <div
-            key={index}
-            className={`cell ${item}`}
-            onClick={() => handleCellClick(index)}
-          >
-            {item}
-          </div>
-        ))}
+      <div className="game">
+        <h1 className="title">Jogo da Velha</h1>
+        <h3>Vez do Jogador {currentPlayer}</h3>
+        {winner && (
+          <header>
+            {winner === 'E' ? (
+              <h2 className="winner-message">
+                <span className={winner}>Empatou!</span>
+              </h2>
+            ) : (
+              <h2 className="winner-message">
+                <span className={winner}>{winner}</span> venceu!
+              </h2>
+            )}
+            <button type="button" onClick={resetGame}>
+              Reiniciar
+            </button>
+          </header>
+        )}
+        <div className={`board ${winner && 'game-over'}`}>
+          {board.map((item, index) => (
+            <div
+              key={index}
+              className={`cell ${item}`}
+              onClick={() => handleCellClick(index)}
+            >
+              {item}
+            </div>
+          ))}
+        </div>
       </div>
-      {winner && (
-        <footer>
-          {winner === 'E' ? (
-            <h2 className="winner-message">
-              <span className={winner}>Empatou!</span>
-            </h2>
-          ) : (
-            <h2 className="winner-message">
-              <span className={winner}>{winner}</span> venceu!
-            </h2>
-          )}
-          <button type="button" onClick={resetGame}>
-            Reiniciar
-          </button>
-        </footer>
-      )}
+      <aside>
+        <h2>Placar</h2>
+        <header>
+          <p>
+            <span className={PlayerSymbol.PLAYER1}>
+              Jogador {PlayerSymbol.PLAYER1}:{' '}
+            </span>
+            {getTotalVictory(PlayerSymbol.PLAYER1)}
+          </p>
+          x
+          <p>
+            <span className={PlayerSymbol.PLAYER2}>
+              Jogador {PlayerSymbol.PLAYER2}:{' '}
+            </span>
+            {getTotalVictory(PlayerSymbol.PLAYER2)}:
+          </p>
+          |
+          <span className={PlayerSymbol.DRAW}>
+            Empate {PlayerSymbol.DRAW}: {getTotalVictory(PlayerSymbol.DRAW)}
+          </span>
+        </header>
+        <div>
+          <h4>Histórico</h4>
+        </div>
+        {history.map((item, index) => (
+          <p key={index} className={item}>
+            Round {index + 1} - Vencedor = {item}
+          </p>
+        ))}
+      </aside>
     </main>
   );
 }
